583dc4784c06026dad70d248e38486d34b49d491
Lakeserl <dinhlam18112003@gmail.com>
2025-07-07 22:39:32 +0700

    Add SideBar with Reactive Form

--- HotelWeb/src/app/app.config.ts
+++ HotelWeb/src/app/app.config.ts
@@ -1,16 +1,20 @@
-import { provideEventPlugins } from "@taiga-ui/event-plugins";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { vi_VN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import vi from '@angular/common/locales/vi';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(vi);
 
 export const appConfig: ApplicationConfig = {
   providers: [
-        provideAnimations(),
-        provideBrowserGlobalErrorListeners(),
+    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-        provideEventPlugins()
-    ]
+    provideRouter(routes), provideNzI18n(vi_VN), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()
+  ]
 };
--- HotelWeb/src/app/app.ts
+++ HotelWeb/src/app/app.ts
@@ -1,10 +1,14 @@
-import { TuiRoot } from "@taiga-ui/core";
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, NzLayoutModule, ReactiveFormsModule, BrowserAnimationsModule, FormsModule, BrowserModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
